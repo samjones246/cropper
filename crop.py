dd24f4ce2e22b103ff543b50fc53f4e554ffb6f1
Sam Jones <sam.jones240699@gmail.com>
2021-09-08 19:56:47 +0100

    added support for tablet resolution, plus res detection and error handling

--- crop.py
+++ crop.py
@@ -6,8 +6,10 @@ import os
 dest = "cropped"
 ext = "jpg"
 prefix = "pg"
-top = 91
-bottom = 2269
+dims = {
+    (1080, 2400): (91, 2269),
+    (1200, 2000): (36, 1927)
+}
 
 def main():
     # Create output folder if it is not present
@@ -18,12 +20,26 @@ def main():
     paths = glob(f"./*.{ext}")
 
     # Iterate over input files, crop and save
+    success = 0
+    fail = 0
     for i, path in enumerate(paths):
         img = Image.open(path)
+        if img.size not in dims:
+            print(f"Unsupported resolution {img.size[0]}x{img.size[1]} for file '{path}', skipping")
+            fail += 1
+            continue
+        top, bottom = dims[img.size]
         img = img.crop((0, top, img.width, bottom))
         fname = f"{prefix}{i+1}.{ext}"
         img.save(f"{dest}/{fname}")
+        success += 1
+    print(f"Done: {success} files processed, {fail} skipped")
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        print(e)
+    finally:
+        input("Press any key to finish...")
