cc9eceb77107db4520d6ae82121e9922ce57e491
Sam Jones <sam.jones240699@gmail.com>
2021-09-09 20:55:33 +0100

    compression and quality approximator (needs testing)

--- crop.py
+++ crop.py
@@ -1,6 +1,7 @@
 from PIL import Image
 from glob import glob
 import os
+from math import floor
 
 # Constants
 dest = "cropped"
@@ -10,6 +11,9 @@ dims = {
     (1080, 2400): (91, 2269),
     (1200, 2000): (36, 1927)
 }
+target_bytes_total = 100000000 # Desired total size for output files
+temp_file_name = "__temp__"
+variance = 0.05
 
 def main():
     # Create output folder if it is not present
@@ -23,16 +27,39 @@ def main():
     # Iterate over input files, crop and save
     success = 0
     fail = 0
+    quality = -1
+    target_bytes_file = (target_bytes_total / len(paths)) * (1-variance)
+    print(f"Target file size: {int(target_bytes_file / 1000)}kb")
     for i, path in enumerate(paths):
         img = Image.open(path)
         if img.size not in dims:
             print(f"Unsupported resolution {img.size[0]}x{img.size[1]} for file '{path}', skipping")
             fail += 1
             continue
+                
         top, bottom = dims[img.size]
         img = img.crop((0, top, img.width, bottom))
         fname = f"{prefix}{i+1}.{ext}"
-        img.save(f"{dest}/{fname}")
+        
+        if quality == -1: # Quality not yet determined, use this image to compute:
+            quality = 100
+            step = quality
+            direction = -1
+            while step > 0:    
+                img.save(f"{temp_file_name}.{ext}", optimise=True, quality=quality)
+                result = os.path.getsize(f"{temp_file_name}.{ext}")
+                if result < target_bytes_file:
+                    direction = 1
+                else:
+                    direction = -1
+                if quality == 100 and direction == 1:
+                    break
+                step = floor(step / 2)
+                quality += step * direction
+                quality = max(min(quality, 100), 0) # Clamp to [0,100] just in case
+            os.remove(f"{temp_file_name}.{ext}")
+                
+        img.save(f"{dest}/{fname}", optimise=True, quality=quality)
         success += 1
     print(f"Done: {success} files processed, {fail} skipped")
 
