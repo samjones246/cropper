6c2d4423af49acedd9c337e1415be1c62370bca6
Sam Jones <sam.jones240699@gmail.com>
2021-09-10 01:03:16 +0100

    improved quality optimisation algorithm

--- crop.py
+++ crop.py
@@ -2,6 +2,7 @@ from PIL import Image
 from glob import glob
 import os
 from math import floor
+from time import time
 
 # Constants
 dest = "cropped"
@@ -11,7 +12,7 @@ dims = {
     (1080, 2400): (91, 2269),
     (1200, 2000): (36, 1927)
 }
-target_bytes_total = 100000000 # Desired total size for output files
+target_bytes_total = pow(1024, 2) * 100 # Desired total size for output files
 temp_file_name = "__temp__"
 variance = 0.05
 
@@ -22,14 +23,61 @@ def main():
 
     # Get input files
     paths = glob(f"./*.{ext}")
-    print(f"Found {len(paths)} input files, starting...")
+    print(f"Found {len(paths)} input files")
 
-    # Iterate over input files, crop and save
     success = 0
     fail = 0
-    quality = -1
     target_bytes_file = (target_bytes_total / len(paths)) * (1-variance)
-    print(f"Target file size: {int(target_bytes_file / 1000)}kb")
+    estimated_size = target_bytes_file * len(paths)
+    print(f"Target total size: {int(target_bytes_total / pow(1024, 2))}MB")
+    print(f"Target file size: {int(target_bytes_file / 1024)}KB (x{len(paths)} = {int(estimated_size / pow(1024, 2))}MB, {variance * 100}% error margin applied)")
+
+    totalsize = sum(os.path.getsize(p) for p in paths)
+    meansize = totalsize / len(paths)
+    print(f"Mean input file size: {int(meansize / 1024)}kb")
+    avg_file = None
+    best_diff = None
+    for path in paths:
+        img = Image.open(path)
+        if img.size not in dims:
+            continue # Must be able to process the file to use for quality optimisation
+        
+        diff = abs(meansize - os.path.getsize(path))
+        if not avg_file or diff < best_diff:
+            avg_file = path
+            best_diff = diff
+
+    if not avg_file:
+        raise ValueError("Unable to select an image for quality optimisation")
+    
+    print(f"Closest match: {int(os.path.getsize(avg_file) / 1024)}kb ({avg_file})")
+
+    img = Image.open(avg_file)
+    top, bottom = dims[img.size]
+    img = img.crop((0, top, img.width, bottom))
+
+    quality = 100
+    step = quality
+    direction = -1
+    while step > 0:    
+        img.save(f"{temp_file_name}.{ext}", optimise=True, quality=quality)
+        result = os.path.getsize(f"{temp_file_name}.{ext}")
+        if result < target_bytes_file:
+            direction = 1
+        else:
+            direction = -1
+        if quality == 100 and direction == 1:
+            break
+        step = floor(step / 2)
+        quality += step * direction
+        quality = max(min(quality, 100), 0) # Clamp to [0,100] just in case
+    os.remove(f"{temp_file_name}.{ext}")
+    print(f"Quality setting determined: {quality}")
+
+    
+    print("Starting batch operation...")
+    starttime = time()
+    # Iterate over input files, crop and save
     for i, path in enumerate(paths):
         img = Image.open(path)
         if img.size not in dims:
@@ -40,28 +88,10 @@ def main():
         top, bottom = dims[img.size]
         img = img.crop((0, top, img.width, bottom))
         fname = f"{prefix}{i+1}.{ext}"
-        
-        if quality == -1: # Quality not yet determined, use this image to compute:
-            quality = 100
-            step = quality
-            direction = -1
-            while step > 0:    
-                img.save(f"{temp_file_name}.{ext}", optimise=True, quality=quality)
-                result = os.path.getsize(f"{temp_file_name}.{ext}")
-                if result < target_bytes_file:
-                    direction = 1
-                else:
-                    direction = -1
-                if quality == 100 and direction == 1:
-                    break
-                step = floor(step / 2)
-                quality += step * direction
-                quality = max(min(quality, 100), 0) # Clamp to [0,100] just in case
-            os.remove(f"{temp_file_name}.{ext}")
-                
         img.save(f"{dest}/{fname}", optimise=True, quality=quality)
         success += 1
-    print(f"Done: {success} files processed, {fail} skipped")
+    print(f"Done ({time() - starttime:.2f}s elapsed)")
+    print(f"{success} files processed, {fail} skipped")
 
 
 if __name__ == "__main__":
